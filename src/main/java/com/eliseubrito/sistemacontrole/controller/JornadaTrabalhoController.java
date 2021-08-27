5c7a20772a6d4865595621c2f8422293bf89967d
eliseusbrito <eliseusbrito@gmail.com>
2021-08-27 20:36:54 -0300

    Adicionado metodos put e delete, adicionado @Audited e dependencias hibernate e as do lombok faltantes

--- src/main/java/com/eliseubrito/sistemacontrole/controller/JornadaTrabalhoController.java
+++ src/main/java/com/eliseubrito/sistemacontrole/controller/JornadaTrabalhoController.java
@@ -31,4 +31,20 @@ public class JornadaTrabalhoController {
         return  ResponseEntity.ok(jornadaService.getById(idJornada).orElseThrow(() -> new NoSuchElementException("Not found!")));
     }
 
+    @PutMapping
+    public JornadaTrabalho updateJornada(@RequestBody JornadaTrabalho jornadaTrabalho){
+        return jornadaService.updateJornada(jornadaTrabalho);
+    }
+
+    @DeleteMapping("/{idJornada}")
+    public ResponseEntity deleteByID(@PathVariable("idJornada") Long idJornada) throws Exception {
+        try {
+            jornadaService.deleteJornada(idJornada);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return (ResponseEntity<JornadaTrabalho>) ResponseEntity.ok();
+
+    }
+
 }
--- src/main/java/com/eliseubrito/sistemacontrole/model/JornadaTrabalho.java
+++ src/main/java/com/eliseubrito/sistemacontrole/model/JornadaTrabalho.java
@@ -1,6 +1,7 @@
 package com.eliseubrito.sistemacontrole.model;
 
 import lombok.*;
+import org.hibernate.envers.Audited;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,6 +14,7 @@ import javax.persistence.Id;
 @EqualsAndHashCode
 @Builder
 @Entity
+@Audited
 public class JornadaTrabalho {
     @Id
     @GeneratedValue
